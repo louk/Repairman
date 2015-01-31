185858945b7a2aeff12d97de234236ca294a96e7
Louk <louk@Louks-MacBook-Pro.local>
2015-01-31 14:52:26 +0800

    Initial Commit

---
+++ Repairman/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Repairman
+//
+//  Created by Louk on 1/31/15.
+//  Copyright (c) 2015 Louk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Repairman/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Repairman
+//
+//  Created by Louk on 1/31/15.
+//  Copyright (c) 2015 Louk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
